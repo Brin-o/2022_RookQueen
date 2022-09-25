32c09eb8e9148e253cff40158a7a4ffd3b4bc369
Fer <56074355+fralvarezz@users.noreply.github.com>
2022-09-25 19:59:09 +0200

    Bishop attack

--- Scripts/Bishop.gd
+++ Scripts/Bishop.gd
@@ -13,8 +13,25 @@ func can_move_to():
 		while boardScene.is_steppable(next_tile):
 			if next_tile != current_tile:
 				tiles.push_back(next_tile)
-			if boardScene.get_tile(next_tile).contains_enemy():
+			if boardScene.get_tile(next_tile).contains != null and boardScene.get_tile(next_tile).contains != self:
+				tiles.remove(len(tiles)-1)
 				break
 			next_tile += dir
 
 	return tiles
+
+
+func move_to(var pos : Vector2):
+	var directions = [Vector2(-1,-1), Vector2(-1,1), Vector2(1,-1), Vector2(1,1)]
+	.move_no_turn(pos)
+
+	for dir in directions:
+		var attack_pos = pos + dir
+		if boardScene.is_inbounds(attack_pos):
+			if boardScene.get_tile(attack_pos).contains_opponent(type):
+				var damage : int = round(rand_range(min_damage, max_damage))
+				var killed = boardScene.get_tile(attack_pos).contains.take_damage(damage)
+    
+	if type == "Player":
+		yield(self, "finished_movement")
+		GameManager.next_turn()
