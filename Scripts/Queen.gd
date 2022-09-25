dda13579902589c46bb0a3a5c13b9bf9303bc221
Fer <56074355+fralvarezz@users.noreply.github.com>
2022-09-26 01:19:31 +0200

    Queen

--- Scripts/Queen.gd
+++ Scripts/Queen.gd
@@ -2,6 +2,9 @@ extends BasePiece
 
 class_name Queen
 
+export var counter_increase : int = 2
+var counter : int = 0
+
 func can_move_to():
 	var tiles : Array = []
 
@@ -14,8 +17,25 @@ func can_move_to():
 		while boardScene.is_steppable(next_tile):
 			if next_tile != current_tile:
 				tiles.push_back(next_tile)
-			if boardScene.get_tile(next_tile).contains_enemy():
+
+			if boardScene.get_tile(next_tile).contains != null:
+				if boardScene.get_tile(next_tile).contains != self and boardScene.get_tile(next_tile).contains_ally(type):
+					tiles.remove(len(tiles)-1)
+			if boardScene.get_tile(next_tile).contains_opponent(type):
 				break
 			next_tile += dir
 
 	return tiles
+
+func move_to(var pos : Vector2):
+	if boardScene.get_tile(pos).contains_opponent(type):
+		var attack_position = boardScene.board_position(pos)
+		var damage : int = round(rand_range(min_damage + counter*counter_increase, max_damage + counter*counter_increase))
+		var killed = boardScene.get_tile(pos).contains.take_damage(damage)
+		if killed:
+			.move_to(pos)
+		else:
+			.attack(boardScene.board_position(current_tile), attack_position)
+			counter+=1
+	else:
+		.move_to(pos)
