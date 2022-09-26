2cf898eb11435160cdde13e7702ef8f348c9878c
Brin <49071768+Brin-o@users.noreply.github.com>
2022-09-26 11:52:35 +0200

    unit hp shadow

--- Scripts/UnitHP.gd
+++ Scripts/UnitHP.gd
@@ -6,6 +6,9 @@ onready var piece = get_parent()
 func _ready():
 	if piece.type == "Player":
 		$Label.visible = false
+		$Shadow.visible = false
 
 func _process(delta):
 	$Label.text = str(piece.hp) + "hp" 
+	$Shadow.text=  str(piece.hp) + "hp" 
+
